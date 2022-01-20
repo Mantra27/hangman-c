f055f87fc58a445f9930089c18f15d4dd3c536c9
Mantra Gohil <75286433+Mantra27@users.noreply.github.com>
2022-01-20 16:27:57 +0530

    Update work.h

--- work.h
+++ work.h
@@ -1,57 +1,43 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
-int length(int *input){
-    int i = 0;
+
+int getLength(char* IN[]){
+    int index = 0;
     while(1){
-        if(input[i] == 0){
-            return i;
-        }
-        else if(input[i] == NULL){
-            return i;
-        }
-        else if(input[i] == " "){
-            return i;
+        if(IN[index] == NULL){
+            return index;
         }
         else{
-            i++;
+            index++;
         }
-    };
-};
-int letters(char *input){
-    int i = 0;
-    while(1){
-        if(input[i] == ""){
-            return i;
-        }
-        else{
-            i++;
-        }
-    };
+    }
 };
 
-void *remain(int i){
+char *stickman(int i){
     if(i>5){
         return "invalid input chance---";
     }
     else if(i==0){
-        return "   +---+\n   |   |\n   O   |\n  /|\\  |\n  / \\  |\n       |\n   =========";
+        printf("   +---+\n   |   |\n   O   |\n  /|\\  |\n  / \\  |\n       |\n   =========");
     }
     else if(i==1){
-        return "   +---+\n   |   |\n   O   |\n  /|\\  |\n       |\n       |\n   =========";
+        printf("   +---+\n   |   |\n   O   |\n  /|\\  |\n       |\n       |\n   =========");
     }
     else if(i==2){
-        return "   +---+\n   |   |\n   O   |\n   |   |\n       |\n       |\n   =========";
+        printf("   +---+\n   |   |\n   O   |\n   |   |\n       |\n       |\n   =========");
     }
     else if(i==3){
-        return "   +---+\n   |   |\n   O   |\n       |\n       |\n       |\n   =========";
+        printf("   +---+\n   |   |\n   O   |\n       |\n       |\n       |\n   =========");
     }
     else if(i==4){
-        return "   +---+\n   |   |\n       |\n       |\n       |\n       |\n   =========";
+        printf("   +---+\n   |   |\n       |\n       |\n       |\n       |\n   =========");
     }
     else if(i==5){
-        return "   +---+\n       |\n       |\n       |\n       |\n       |\n   =========";
+        printf("   +---+\n       |\n       |\n       |\n       |\n       |\n   =========");
     }
+    return 0;
 };
 
 int getRand(int lower, int upper)
